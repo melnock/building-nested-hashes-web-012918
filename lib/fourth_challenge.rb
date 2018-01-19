1e81b1aec3590aaa4575d55b2682bcf924bbcc00
Melissa Nock <melnock@gmail.com>
2018-01-19 02:07:35 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,6 +1,6 @@
 def fourth_challenge
   epic_tragedy = {
-   :patriarch =>{:name => "Lord Montague", :age => "53"}, 
+   :montague => {:patriarch =>{:name => "Lord Montague", :age => "53"}, 
       :matriarch =>{:name => "Lady Montague", :age => "54"}, 
       :hero =>{:name => "Romeo", :age => "15", :status => "alive"}, 
       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
